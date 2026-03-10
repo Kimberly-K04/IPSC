2248bbe9259d239f7ceda2df5c52b0a5ba599fc3
Ashlin Rofina <ashlinrofina@gmail.com>
2026-03-10 17:26:43 +0300

    adds relationship and serialize_rules

--- server/models/order.py
+++ server/models/order.py
@@ -5,12 +5,15 @@ from sqlalchemy.orm import relationship
 class Order(db.Model, SerializerMixin):
     __tablename__ = "orders" 
 
-    serialize_rules = ("-") 
+    serialize_rules = ("-sales.order") 
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer,  db.ForeignKey("users.id"), nullable=False)
     order_date = db.Column(db.DateTime, server_default=db.func.now())
     status = db.Column(db.String, nullable=False)
     total_amount = db.Column(db.Numeric, nullable=False) 
+
+    sales = db.relationship("Sale", back_populates="order", cascade="all, delete_orphan") 
+
 
 
