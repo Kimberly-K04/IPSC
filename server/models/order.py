b85d5030160ed254cea8e52cd7987bd490e9af17
Ashlin Rofina <ashlinrofina@gmail.com>
2026-03-12 13:22:47 +0300

    fixed the db error

--- server/models/order.py
+++ server/models/order.py
@@ -1,19 +1,24 @@
-from server.app import db
+
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import  relationship 
+from ..config import db
 
-class Order(db.Model, SerializerMixin):
+
+class Order(db.Model, SerializerMixin): 
+
     __tablename__ = "orders" 
+    
+    serialize_rules = ("-sales.order,")  
+   
 
-    serialize_rules = ("-sales.order,") 
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer,  db.ForeignKey("users.id"), nullable=False)
     order_date = db.Column(db.DateTime, server_default=db.func.now())
     status = db.Column(db.String, nullable=False)
-    total_amount = db.Column(db.Decimal, nullable=False) 
+    total_amount = db.Column(db.Numeric(10), nullable=False) 
 
-    sales = db.relationship("Sale", back_populates="order", cascade="all, delete_orphan") 
+    sales = db.relationship("Sale", back_populates="order", cascade="all, delete-orphan") 
 
 
 
--- server/models/sale.py
+++ server/models/sale.py
@@ -1,8 +1,9 @@
-from server.app import db
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import relationship
 
-class Sale():
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import relationship 
+from ..config import db
+
+class Sale(db.Model, SerializerMixin):
     __tablename__="sales"  
     serialize_rule = ("-order.sales")
 
@@ -10,7 +11,7 @@ class Sale():
     user_id = db.Column(db.Integer,  db.ForeignKey("users.id"), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    total_price = db.Column(db.Decimal, nullable=False)  
+    total_price = db.Column(db.Numeric(10,2), nullable=False)  
 
     order = db.relationship("Order", back_populates="sales")
 
